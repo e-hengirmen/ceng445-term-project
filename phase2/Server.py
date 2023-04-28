2c9934f6a515960f9a12e382d06080e50885525c
e-hengirmen <ehengirmen@hotmail.com>
2023-04-28 14:20:49 +0300

    game board choice

--- phase2/Server.py
+++ phase2/Server.py
@@ -13,7 +13,7 @@ line test application demonstrating all features of your library.
 #-------------------------------------------------------------------------------------
 
 class Server:
-    game_board_filename = "../gameBoards/deneme3_in"
+    game_board_filename = "../gameBoards/deneme_in"
 
     def __init__(self):
         self.monopoly_instances = {}
