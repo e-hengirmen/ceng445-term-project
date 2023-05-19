0a4538b8ea027e762919400f462859380475e9d0
e-hengirmen <ehengirmen@hotmail.com>
2023-05-19 22:42:24 +0300

    Taking game data from games

--- phase3/server/views.py
+++ phase3/server/views.py
@@ -28,7 +28,7 @@ def addgame(request):
             monopoly_model_object.user_count=int(user_count)
             monopoly_model_object.save()
 
-            monopoly=Board("gameBoards/deneme_in",number_of_users=4)
+            monopoly=Board("gameBoards/deneme_in",number_of_users=int(user_count))
             board_dict[monopoly_model_object.game_id]=monopoly
 
         # return render(request, 'server/home.html', {"games":GAME.objects.all(),"message":message})
@@ -45,8 +45,10 @@ def join(request):
 # @method_decorator(login_required, name='dispatch')
 class ServerView(View):
     def get(self, request):
-        states=[board_dict[game.game_id].getboardstate() for game in GAME.objects.all()]
-        return render(request, 'server/home.html', {"games":GAME.objects.all(),"states":states})
+        # states=[board_dict[game.game_id].getboardstate() for game in GAME.objects.all()]
+        representation=[board_dict[game.game_id].__repr__() for game in GAME.objects.all()]
+        states=        [not board_dict[game.game_id].WaitingState for game in GAME.objects.all()]
+        return render(request, 'server/home.html', {"id_n_repr":zip(GAME.objects.all(),representation,states)})
 
 # def index(request):
 #     return render(request, 'home.html')
